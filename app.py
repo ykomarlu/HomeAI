970eb5195182f987b076b3dd5017aeb9dc7b8ed6
Yushus Komarlu <yushusgk@gmail.com>
2025-01-02 21:31:11 -0500

    database init

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+# bcyrpt object initialization
 
-class name(db.Model):
-    id = db.Column(db.Integer, primary_key=True  )
+class Users(db.Model):
+    userId = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(15), unique=True, nullable=False)
+    password = db.Column(db.String(15), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"userName: {userName}, password: {password}"
+    # basically this function acts as the to-string for the Users python object
+    # helps with debugging
 
 # make the db connection
 # the way that the database connection is
